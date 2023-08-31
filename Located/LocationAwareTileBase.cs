3dbcf7515ec56adae8de3710b78a7dfff7058865
kamieniarzadam <143035840+kamieniarzadam@users.noreply.github.com>
2023-08-31 20:54:06 +0200

    Wrap tile GO.

--- Located/LocationAwareTileBase.cs
+++ Located/LocationAwareTileBase.cs
@@ -4,11 +4,28 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu()]
 public class LocationAwareTileBase : PrefabTileBase
 {
+    GameObject wrapper;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+        if (!wrapper)
+        {
+            wrapper = new GameObject("Tile Wrapper");
+            wrapper.SetActive(false);
+            wrapper.transform.parent = tilemap.GetComponent<TilemapRenderer>().gameObject.transform;
+            wrapper.hideFlags = HideFlags.DontSave;
+            wrapper.AddComponent<LocationOnTilemapHelper>();
+            Instantiate(prefab, wrapper.transform);
+        }
+        wrapper.SetActive(false);
+        tileData.gameObject = wrapper;
+    }
 
     public override bool StartUp(Vector3Int location, ITilemap tilemap, GameObject go)
     {
         var ret = base.StartUp(location, tilemap, go);
-        go.AddComponent<LocationOnTilemapHelper>().Location = location;
+        go.GetComponent<LocationOnTilemapHelper>().Location = location;
         go.SetActive(true);
         return ret;
     }
